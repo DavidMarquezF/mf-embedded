076fe9b81d81365b1106d9db6cfc4fd23a2ccbc3
David <dmarquez.estudis@gmail.com>
2021-06-07 17:20:07 +0200

    removed switch actuator

--- src/components/include/mf_switch_actuator.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MF_SWITCH_ACTUATOR_H_
-#define _MF_SWITCH_ACTUATOR_H_
-#include <stdint.h>
-#include "oc_api.h"
-
-uint8_t mf_switch_actuator_create_resource(oc_resource_t *temp_resource);
-uint8_t mf_switch_actuator_init(void);
-uint8_t mf_switch_actuator_destroy(void);
-#endif
