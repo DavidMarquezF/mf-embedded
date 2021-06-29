c19fb0d3fe9103ee5dfeccb45951b622601fe2ee
David <dmarquez.estudis@gmail.com>
2021-06-29 08:50:01 +0200

    improv(debug): Added debug info to i2c discover

--- src/components/mf_i2c_device/mf_i2c_device.c
+++ src/components/mf_i2c_device/mf_i2c_device.c
@@ -3,11 +3,13 @@
 
 mf_device_t devices[MF_I2C_MAX_DEVICES];
 uint8_t mf_i2c_device_discover_devices(void){
+    PRINT("\nDiscovering I2C devices...\n");
     uint8_t index=0;
     //TODO: Improve this by just including the existing i2c modules
     for(uint8_t i = 1; i < 127; i++){
         if(mf_i2c_send_message(i, NULL, 0) == 0){
             devices[index++] = i;
+            PRINT("\tId: %d\n", i);
             if(index >= MF_I2C_MAX_DEVICES)
                 break;
         }
